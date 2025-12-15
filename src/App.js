a29036f2b9f56c4632a1137627299c7e25531655
David Fabrizi <137659008+davidfabkj12@users.noreply.github.com>
2025-12-15 12:00:54 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import axios from 'axios';
-import '@/App.css';
+import './App.css';
 import { Toaster } from '@/components/ui/sonner';
 
 // Context
